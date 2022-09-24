186449bcd64d273feab08100dd53f23891840fe2
Timm Friebe <thekid@thekid.de>
2022-09-24 14:18:53 +0200

    Extract hashing into member variable

--- src/main/php/de/thekid/dialog/Repository.php
+++ src/main/php/de/thekid/dialog/Repository.php
@@ -6,6 +6,7 @@ use text\hash\Hashing;
 use util\{Date, Secret};
 
 class Repository {
+  private $hashing= Hashing::sha256();
 
   public function __construct(private Database $database) { }
 
@@ -13,7 +14,7 @@ class Repository {
   public function authenticate(string $user, Secret $secret): ?Document {
     $cursor= $this->database->collection('users')->find([
       'handle' => $user,
-      'hash'   => Hashing::sha256()->digest($secret->reveal())->hex()
+      'hash'   => $this->hashing->digest($secret->reveal())->hex()
     ]);
     return $cursor->first();
   }
