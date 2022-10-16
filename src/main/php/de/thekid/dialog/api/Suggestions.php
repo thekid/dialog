fbc0c07ea84cacdf8274f75a0936abea95d977b3
Timm Friebe <thekid@thekid.de>
2022-10-16 17:56:32 +0200

    Add suggestion API

---
+++ src/main/php/de/thekid/dialog/api/Suggestions.php
@@ -0,0 +1,27 @@
+<?php namespace de\thekid\dialog\api;
+
+use de\thekid\dialog\Repository;
+use web\rest\{Resource, Get, Param};
+
+#[Resource('/api/suggestions')]
+class Suggestions {
+
+  public function __construct(private Repository $repository) { }
+
+  #[Get]
+  public function suggest(#[Param] $q) {
+    foreach ($this->repository->suggest(trim($q)) as $suggestion) {
+      yield [
+        'title' => htmlspecialchars($suggestion['title']),
+        'date'  => $suggestion['date']->toString('d.m.Y'),
+        'link'  => (isset($suggestion['is']['journey'])
+          ? '/journey/'.$suggestion['slug']
+          : (isset($suggestion['parent']) 
+            ? '/journey/'.strtr($suggestion['slug'], ['/' => '#'])
+            : '/content/'.$suggestion['slug']
+          )
+        )
+      ];
+    }
+  }
+}
