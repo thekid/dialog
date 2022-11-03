5d2a31f7f298702b5f877a90de3fd4b0f2b0ec89
Timm Friebe <thekid@thekid.de>
2022-11-03 21:48:00 +0100

    Simplify test code

--- src/test/php/de/thekid/dialog/unittest/HelpersTest.php
+++ src/test/php/de/thekid/dialog/unittest/HelpersTest.php
@@ -4,11 +4,7 @@ use de\thekid\dialog\Helpers;
 use unittest\{Assert, Test, Values};
 
 class HelpersTest {
-
-  #[Test]
-  public function can_create() {
-    new Helpers();
-  }
+  private $helpers= [...new Helpers()->helpers()];
 
   #[Test, Values([
     [['2022-11-29', '2022-12-01', 'format' => 'd.m.Y'], '29.11.2022 - 01.12.2022'],
@@ -17,17 +13,15 @@ class HelpersTest {
     [['2022-11-29', '2022-11-30', 'format' => 'M Y'], 'Nov 2022'],
   ])]
   public function range($options, $expected) {
-    $range= [...new Helpers()->helpers()]['range'];
-    Assert::equals($expected, $range(null, null, $options));
+    Assert::equals($expected, $this->helpers['range'](null, null, $options));
   }
 
   #[Test, Values([
-    [[['slug' => 'test', 'is' => ['journey' => true]]], 'journey/test'],
-    [[['slug' => 'test', 'is' => ['content' => true]]], 'content/test'],
-    [[['slug' => 'test/child', 'parent' => 'test', 'is' => ['content' => true]]], 'journey/test#child'],
+    [['slug' => 'test', 'is' => ['journey' => true]], 'journey/test'],
+    [['slug' => 'test', 'is' => ['content' => true]], 'content/test'],
+    [['slug' => 'test/child', 'parent' => 'test', 'is' => ['content' => true]], 'journey/test#child'],
   ])]
-  public function route($options, $expected) {
-    $route= [...new Helpers()->helpers()]['route'];
-    Assert::equals($expected, $route(null, null, $options));
+  public function route($entry, $expected) {
+    Assert::equals($expected, $this->helpers['route'](null, null, [$entry]));
   }
 }
