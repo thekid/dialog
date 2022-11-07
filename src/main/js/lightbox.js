a7ad81de98e7a5693ad209d567a1dc2fa04ad2af
Timm Friebe <thekid@thekid.de>
2022-11-07 23:53:46 +0100

    Migrate lightbox to ES6 classes

--- src/main/js/lightbox.js
+++ src/main/js/lightbox.js
@@ -1,32 +1,48 @@
-const lightbox = {
-  $element : document.querySelector('#lightbox'),
-  show     : function($link) {
-    const $full = lightbox.$element.querySelector('img');
+class Lightbox {
+
+  /** Opens the given lightbox, loading the image and filling in meta data */
+  #open($target, $link) {
+    const $full = $target.querySelector('img');
     $full.src = '';
-    lightbox.$element.classList.add('open');
+    $target.classList.add('open');
     $full.src = $link.href;
 
     // Overlay meta data if present
     const $img = $link.querySelector('img');
-    const $meta = lightbox.$element.querySelector('.meta');
+    const $meta = $target.querySelector('.meta');
     if ('' !== ($img.dataset.make ?? '')) {
       $meta.querySelectorAll('output').forEach($o => $o.value = $img.dataset[$o.name]);
       $meta.style.visibility = 'visible';
     } else {
       $meta.style.visibility = 'hidden';
     }
-
-    return false;
-  },
-  close    : function() {
-    lightbox.$element.classList.remove('open');
   }
-};
+
+  /** Closes the given lightbox */
+  #close($target) {
+    $target.classList.remove('open');
+  }
+
+  /** Attach all of the given elements to open the lightbox specified by the given DOM element */
+  attach(selector, $target) {
+    document.addEventListener('keydown', e => {
+      if ('Escape' === e.key) this.#close($target);
+    });
+
+    $target.addEventListener('click', e => {
+      this.#close($target);
+    });
+
+    selector.forEach($link => $link.addEventListener('click', e => {
+      e.preventDefault();
+      this.#open($target, $link);
+    }));
+  }
+}
 
 document.onkeydown = function(e) {
   switch (e.key) {
     case 'ArrowRight': document.location.href = document.querySelector('a.next').href; break;
     case 'ArrowLeft': document.location.href = document.querySelector('a.previous').href; break;
-    case 'Escape': lightbox.close(); break;
   }
 };
