aae2cc48c35c8ef8fc4f7a2a97fb1b1707c61f75
Timm Friebe <thekid@thekid.de>
2022-11-13 21:05:29 +0100

    Add more tests for Signing class

--- src/test/php/de/thekid/dialog/unittest/SigningTest.php
+++ src/test/php/de/thekid/dialog/unittest/SigningTest.php
@@ -28,6 +28,31 @@ class SigningTest {
     Assert::true($s->verify($query, $signature));
   }
 
+  #[Test]
+  public function does_not_verify_empty_signature() {
+    $s= new Signing($this->secret);
+
+    Assert::false($s->verify('target=test', ''));
+  }
+
+  #[Test]
+  public function does_not_verify_differing_query() {
+    $s= new Signing($this->secret);
+
+    $signature= $s->sign('target=test');
+    Assert::false($s->verify('target=other', $signature));
+  }
+
+  #[Test]
+  public function does_not_verify_differing_signature() {
+    $s= new Signing($this->secret);
+    $query= 'target=test';
+
+    $signature= $s->sign($query);
+    [$signature[0], $signature[1]]= [$signature[1], $signature[0]];
+    Assert::false($s->verify($query, $signature));
+  }
+
   #[Test, Values([-3600, -1, 1, 3600])]
   public function cannot_verify_links_after_expiration_window($window) {
     $s= new Signing($this->secret)->tolerating(0);
