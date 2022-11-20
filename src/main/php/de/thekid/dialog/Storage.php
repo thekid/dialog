bbb48b631698553b9e681b633dcfaac43099bf9b
Timm Friebe <thekid@thekid.de>
2022-11-20 17:35:02 +0100

    Accept varargs in folder(), add test

--- src/main/php/de/thekid/dialog/Storage.php
+++ src/main/php/de/thekid/dialog/Storage.php
@@ -3,10 +3,11 @@
 use io\Folder;
 use web\handler\FilesFrom;
 
+/** @test de.thekid.dialog.unittest.StorageTest */
 class Storage extends FilesFrom {
 
   /** Returns folder for a given entry */
-  public function folder(string $entry): Folder {
-    return new Folder($this->path(), 'image', $entry);
+  public function folder(string... $args): Folder {
+    return new Folder($this->path(), 'image', ...$args);
   }
 }
