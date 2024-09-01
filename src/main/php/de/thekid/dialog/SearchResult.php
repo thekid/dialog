f970e98262412ef5641584995e6d70fc0bf633be
Timm Friebe <thekid@thekid.de>
2024-09-01 10:37:00 +0200

    Static properties cannot be readonly

--- src/main/php/de/thekid/dialog/SearchResult.php
+++ src/main/php/de/thekid/dialog/SearchResult.php
@@ -3,11 +3,11 @@
 use com\mongodb\Document;
 
 /** @see de.thekid.dialog.Repository::search */
-readonly class SearchResult {
+class SearchResult {
   public static $EMPTY= new self(new Document(), []);
 
   public function __construct(
-    public Document $meta,
-    public iterable $documents,
+    public readonly Document $meta,
+    public readonly iterable $documents,
   ) { }
 }
