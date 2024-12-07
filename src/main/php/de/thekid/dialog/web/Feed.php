638b87f94af6e31b618323cdb9fd389f51cee096
Timm Friebe <thekid@thekid.de>
2024-12-07 10:02:16 +0100

    Add Atom feed

--- src/main/php/de/thekid/dialog/web/Feed.php
+++ src/main/php/de/thekid/dialog/web/Feed.php
@@ -1,7 +1,7 @@
 <?php namespace de\thekid\dialog\web;
 
 use de\thekid\dialog\{Repository, Pagination};
-use web\frontend\{Handler, Get, Param};
+use web\frontend\{Handler, Get, Param, View};
 
 #[Handler('/feed')]
 class Feed {
@@ -12,5 +12,13 @@ class Feed {
   #[Get]
   public function listing(#[Param] $page= 1) {
     return $this->repository->entries($this->pagination, (int)$page);
+  }
+
+  #[Get('/atom')]
+  public function atom() {
+    return View::named('atom')
+      ->header('Content-Type', 'application/atom+xml; charset=utf-8')
+      ->with(['items' => $this->repository->newest(20)])
+    ;
   }
 }
