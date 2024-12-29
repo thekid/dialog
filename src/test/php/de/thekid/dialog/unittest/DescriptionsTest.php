de19e3c449e6bc7c8f11ccacbc7baa21952f366d
Timm Friebe <thekid@thekid.de>
2024-12-29 13:30:28 +0100

    Verify timezone support for description files

--- src/test/php/de/thekid/dialog/unittest/DescriptionsTest.php
+++ src/test/php/de/thekid/dialog/unittest/DescriptionsTest.php
@@ -4,6 +4,7 @@ use de\thekid\dialog\import\{Description, Descriptions};
 use io\streams\MemoryInputStream;
 use lang\FormatException;
 use test\{Assert, Expect, Test, Values};
+use util\{Date, TimeZone};
 
 class DescriptionsTest {
 
@@ -34,6 +35,18 @@ class DescriptionsTest {
     Assert::equals(
       '<p>Content</p>',
       $this->parse("---\ntitle: Test\nlocation: KA\n---\nContent")->content
+    );
+  }
+
+  #[Test, Values([
+    ['', TimeZone::getLocal()],
+    ['Europe/Berlin', TimeZone::getByName('Europe/Berlin')],
+    ['Asia/Muscat', TimeZone::getByName('Asia/Muscat')],
+  ])]
+  public function resolve_timezone($tz, $resolved) {
+    Assert::equals(
+      ['date' => new Date('2024-12-29 13:19:00', $resolved)],
+      $this->parse("---\ndate: 2024-12-29 13:19:00 {$tz}\n---\nContent")->meta
     );
   }
 }
