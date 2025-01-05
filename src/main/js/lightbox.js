7547a49cd521b9b70e3f8482a4e148490deab243
Timm Friebe <thekid@thekid.de>
2025-01-05 14:00:46 +0100

    Include videos in lightbox

--- src/main/js/lightbox.js
+++ src/main/js/lightbox.js
@@ -12,30 +12,42 @@ class Lightbox {
     }
   }
 
+  /** Returns a preview image for the given media item */
+  #preview($media, offset) {
+    const $item = $media.item(this.#wrap(offset, $media.length));
+    if ($item instanceof HTMLImageElement) {
+      return $item.src;
+    } else if ($item instanceof HTMLVideoElement) {
+      return $item.poster;
+    }
+  }
+
   /** Opens the given lightbox, loading the image and filling in meta data */
-  #open($target, $links, offset) {
-    offset = this.#wrap(offset, $links.length);
-    const $display = $target.querySelector('img.display');
-    const $link = $links.item(offset);
-    const $img = $link.querySelector('img');
+  #open($target, $media, offset) {
+    offset = this.#wrap(offset, $media.length);
+    const $display = $target.querySelector('.display');
+    const $item = $media.item(offset);
     const $meta = $target.querySelector('.meta');
 
     // Update meta information
+    $meta.querySelectorAll('output').forEach($o => $o.value = $item.dataset[$o.name]);
+
+    // Load image, then replace by larger version after a short duration
     $target.dataset.offset = offset;
-    if ('' !== ($img.dataset.make ?? '')) {
-      $meta.querySelectorAll('output').forEach($o => $o.value = $img.dataset[$o.name]);
-      $meta.style.visibility = 'visible';
+    if ($item instanceof HTMLImageElement) {
+      $meta.className = 'meta for-image';
+      $display.innerHTML= `<img src="${$item.src}" width="100%">`;
+      this.#replace = setTimeout(() => $display.querySelector('img').src = $item.dataset['full'], 150);
     } else {
-      $meta.style.visibility = 'hidden';
+      $meta.className = 'meta for-video';
+      $display.innerHTML = `<video autoplay playsinline poster="${$item.poster}" width="100%">${$item.innerHTML}</video>`;
+      this.#replace = null;
     }
 
     // Exchange images
-    $display.src = $img.src;
-    $target.querySelector('img.prev').src = $links.item(this.#wrap(offset - 1, $links.length)).querySelector('img').src;
-    $target.querySelector('img.next').src = $links.item(this.#wrap(offset + 1, $links.length)).querySelector('img').src;
-
-    // ...then replace by larger version after a short duration
-    this.#replace = setTimeout(() => $display.src = $link.href, 150);
+    $target.querySelector('.prev').src = this.#preview($media, offset - 1);
+    $target.querySelector('.next').src = this.#preview($media, offset + 1);
+    $target.focus();
   }
 
   /** Attach all of the given elements to open the lightbox specified by the given DOM element */
@@ -46,7 +58,7 @@ class Lightbox {
 
     // Keyboard navigation
     $target.addEventListener('keydown', e => {
-      clearTimeout(this.#replace);
+      this.#replace && clearTimeout(this.#replace);
 
       let offset;
       switch (e.key) {
@@ -66,28 +78,28 @@ class Lightbox {
     let x, y;
     const threshold = 50;
     $target.addEventListener('touchstart', e => {
-      clearTimeout(this.#replace);
+      this.#replace && clearTimeout(this.#replace);
       x = e.touches[0].clientX;
       y = e.touches[0].clientY;
     });
     $target.addEventListener('touchmove', e => {
       const delta = e.changedTouches[0].clientX - x;
-      $target.querySelector('img.display').style.transform = `translate(${delta}px, 0)`;
+      $target.querySelector('.display').style.transform = `translate(${delta}px, 0)`;
       if (delta > 0) {
-        $target.querySelector('img.next').style.visibility = null;
-        $target.querySelector('img.prev').style.visibility = 'visible';
+        $target.querySelector('.next').style.visibility = null;
+        $target.querySelector('.prev').style.visibility = 'visible';
       } else {
-        $target.querySelector('img.next').style.visibility = 'visible';
-        $target.querySelector('img.prev').style.visibility = null;
+        $target.querySelector('.next').style.visibility = 'visible';
+        $target.querySelector('.prev').style.visibility = null;
       }
       e.cancelable && e.preventDefault();
     }, { passive: false });
     $target.addEventListener('touchend', e => {
       const width = e.changedTouches[0].clientX - x;
       const height = e.changedTouches[0].clientY - y;
-      $target.querySelector('img.prev').style.visibility = null;
-      $target.querySelector('img.next').style.visibility = null;
-      $target.querySelector('img.display').style.transform = null;
+      $target.querySelector('.prev').style.visibility = null;
+      $target.querySelector('.next').style.visibility = null;
+      $target.querySelector('.display').style.transform = null;
 
       // Swipe was mostly vertical, ignore
       if (Math.abs(width) <= Math.abs(height)) return;
@@ -105,14 +117,23 @@ class Lightbox {
       this.#open($target, selector, offset);
     });
 
+    $target.addEventListener('close', e => {
+      const $media = $target.querySelector('.display').children[0];
+      if ($media instanceof HTMLVideoElement) {
+        $media.pause();
+      }
+    });
+
     let i = 0;
-    for (const $link of selector) {
+    for (const $item of selector) {
       const offset = i++;
-      $link.addEventListener('click', e => {
-        e.preventDefault();
-        $target.showModal();
-        this.#open($target, selector, offset);
-      });
+      if ($item instanceof HTMLImageElement) {
+        $item.addEventListener('click', e => {
+          e.preventDefault();
+          $target.showModal();
+          this.#open($target, selector, offset);
+        });
+      }
     }
   }
 }
