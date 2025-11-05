95f48ae5de58c780e4e622aa257701ad68ac4543
Timm Friebe <thekid@thekid.de>
2025-11-05 18:33:28 +0200

    Connect markers on map with dotted lines

--- src/main/js/mapping.js
+++ src/main/js/mapping.js
@@ -1,6 +1,7 @@
 class Mapping {
-  #features = [];
+  #coords = [];
   #markers = [];
+  #features = [];
 
   /** Add marker and link at a given position */
   mark(link, lon, lat, name, kind, uri) {
@@ -11,6 +12,7 @@ class Mapping {
     $image.classList.add(kind);
 
     const position = ol.proj.fromLonLat([lon, lat]);
+    this.#coords.push(position);
     this.#markers.push(new ol.Overlay({
       position: position,
       positioning: 'center-center',
@@ -40,6 +42,20 @@ class Mapping {
     }
 
     const features = new ol.source.Vector({features: this.#features});
+
+    // If there is more than one coordinate, connect with dotted lines
+    if (this.#coords.length > 1) {
+      const line = new ol.Feature({geometry: new ol.geom.LineString(this.#coords)});
+      line.setStyle(new ol.style.Style({
+        stroke: new ol.style.Stroke({
+          color: '#666666',
+          width: 3,
+          lineDash: [6, 6]
+        })
+      }));
+      features.addFeature(line);
+    }
+
     map.addLayer(new ol.layer.Vector({source: features}));
     map.getView().fit(features.getExtent(), {padding: [32, 32, 32, 32], minResolution: 30});
 
